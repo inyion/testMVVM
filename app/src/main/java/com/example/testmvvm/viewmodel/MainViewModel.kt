2ecd28c0314c3268ffdae5f4f410ccbb72d930eb
sam <ilinyion@gmail.com>
2020-04-23 17:45:48 +0900

    commit

--- app/src/main/java/com/example/testmvvm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/testmvvm/viewmodel/MainViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.testmvvm.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.testmvvm.RxEvent
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainViewModel: ViewModel() {
     private val _mainTextData = MutableLiveData<String>()
@@ -15,10 +17,21 @@ class MainViewModel: ViewModel() {
     val userViewModel3 = UserViewModel()
 
     fun init() {
+
+
         addDisposable(
             RxEvent.bus
+                .observeOn(Schedulers.newThread())
                 .subscribe { str ->
-                    _mainTextData.value = "hello $str"
+                    try {
+                        Thread.sleep(3000)
+                    } catch (e: Exception) {
+
+                    }
+                    Log.i("test", _mainTextData.toString())
+                    _mainTextData.postValue("hello $str")
+                    Log.i("test", _mainTextData.hasActiveObservers().toString())
+
                 }
         )
         userViewModel1.init("Android")
